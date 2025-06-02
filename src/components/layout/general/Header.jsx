52e4f3f28f501cad94d6cc1a327fbe2e25d64cb3
Felicitas <felicitas.aguerralde@gmail.com>
2025-06-02 17:12:21 -0300

    Agrego header

---
+++ src/components/layout/general/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Nav } from './Nav'
+export const Header = () => {
+  return (
+    
+    <header className="layout__navbar">
+
+            <div classNameName="navbar__header">
+                <a href="#" classNameName="navbar__title">Red Social</a>
+            </div>
+
+            <Nav />
+        </header>  
+  )
+}
