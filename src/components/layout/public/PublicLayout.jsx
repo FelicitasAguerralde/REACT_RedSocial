fdcf92164bc8f603a0b9b654356773fc666ceb08
Felicitas <felicitas.aguerralde@gmail.com>
2025-06-09 18:39:15 -0300

    Public Layout restringido

--- src/components/layout/public/PublicLayout.jsx
+++ src/components/layout/public/PublicLayout.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { Header } from './Header';
+import { useAuth } from '../../../hooks/useAuth';
 
 export const PublicLayout = () => {
+
+  const { auth } = useAuth();
   return (
     <>
       <Header />
+      {/* Contenido principal */}
       <main className="layout__content">
+        { !auth._id ?
         <Outlet />
+        : 
+        <Navigate to="/social" />
+        }
       </main>
     </>
   )
